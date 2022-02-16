aea3e9277e9f7b30ee49d2387d1a6dfa9732cfd8
Jonas Gehrke <jonasegherke@gmail.com>
2022-02-16 11:32:00 +0100

    Added basic design and index page with express SSR

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get("/terminal-commands", (req, res) =>{
+    res.sendFile(__dirname + "/public/terminal.html")
+})
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
