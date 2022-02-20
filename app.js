30baa26fa5fed987abd9e36ac7ae422e7ca91cc6
jonasegehrke <jonasegehrke@gmail.com>
2022-02-20 16:02:18 +0100

    Created tools.html content

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/terminal-commands", (req, res) =>{
     res.sendFile(__dirname + "/public/terminal.html")
 })
 
+app.get("/tools", (req, res) =>{
+    res.sendFile(__dirname + "/public/tools.html")
+})
+
 app.listen(process.env.PORT || 8080, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
