6565780698abd69a80b613ee1ad9a19408c3dbcd
Jonas Gehrke <jonasegherke@gmail.com>
2022-02-23 08:59:04 +0100

    Added SSR for nav & script components, port changed to 3000 for prep to svelte

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express")
-const path = require("path")
+const fs = require("fs")
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+const nav = fs.readFileSync("./public/components/nav.html").toString();
+const scripts = fs.readFileSync("./public/components/scripts.html").toString();
+const frontPage = fs.readFileSync("./public/pages/index.html").toString();
+const terminalPage = fs.readFileSync("./public/pages/terminal.html").toString();
+const toolPage = fs.readFileSync("./public/pages/tools.html").toString();
+
+const frontPagePage = nav.replace('%%HOME%%', 'active') + frontPage + scripts;
+const terminalPagePage = nav.replace('%%TERMINAL%%', 'active') + terminalPage + scripts;
+const toolPagePage = nav.replace('%%TOOLS%%', 'active') + toolPage + scripts;
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/public/index.html");
+    res.send(frontPagePage)
 })
 
 app.get("/terminal-commands", (req, res) =>{
-    res.sendFile(__dirname + "/public/terminal.html")
+    res.send(terminalPagePage)
 })
 
 app.get("/tools", (req, res) =>{
-    res.sendFile(__dirname + "/public/tools.html")
+    res.send(toolPagePage)
 })
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
