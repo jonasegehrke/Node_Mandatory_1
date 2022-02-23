5bfffd50a09a880dba7a7101ccfac89d52d153eb
Jonas Gehrke <jonasegherke@gmail.com>
2022-02-23 11:01:13 +0100

    Created rest.html & ssr.html aswell as a few additions to both terminal.html & tools.html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express")
-const fs = require("fs")
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const nav = fs.readFileSync("./public/components/nav.html").toString();
@@ -7,23 +7,35 @@ const scripts = fs.readFileSync("./public/components/scripts.html").toString();
 const frontPage = fs.readFileSync("./public/pages/index.html").toString();
 const terminalPage = fs.readFileSync("./public/pages/terminal.html").toString();
 const toolPage = fs.readFileSync("./public/pages/tools.html").toString();
+const restPage = fs.readFileSync("./public/pages/rest.html").toString();
+const ssrPage = fs.readFileSync("./public/pages/ssr.html").toString();
 
 const frontPagePage = nav.replace('%%HOME%%', 'active') + frontPage + scripts;
 const terminalPagePage = nav.replace('%%TERMINAL%%', 'active') + terminalPage + scripts;
 const toolPagePage = nav.replace('%%TOOLS%%', 'active') + toolPage + scripts;
+const restPagePage = nav.replace('%%REST%%', 'active') + restPage + scripts;
+const ssrPagePage = nav.replace('%%SSR%%', 'active') + ssrPage + scripts;
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) =>{
-    res.send(frontPagePage)
+    res.send(frontPagePage);
 })
 
 app.get("/terminal-commands", (req, res) =>{
-    res.send(terminalPagePage)
+    res.send(terminalPagePage);
 })
 
 app.get("/tools", (req, res) =>{
-    res.send(toolPagePage)
+    res.send(toolPagePage);
+})
+
+app.get("/rest", (req, res) =>{
+    res.send(restPagePage);
+})
+
+app.get("/ssr", (req, res) =>{
+    res.send(ssrPagePage)
 })
 
 app.listen(process.env.PORT || 3000, function(){
